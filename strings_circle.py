42fd9bcf2787bdd17250ee5a61f07af83948072f
sharpblade4 <sharpblade4@gmail.com>
2019-06-22 10:56:15 +0300

    naive greedy

--- strings_circle.py
+++ strings_circle.py
@@ -2,20 +2,22 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-from typing import List, Tuple
+from typing import List, Tuple, Union
 from scipy import interpolate
 from PIL import Image
+import time
 
 RADIUS = 50  # cm
 SCREWS_AMOUNT = 470
 STRING_LENGTH = 5.5 * 100 * 1000  # cm
+STRING_LENGTH = 5.5 * 1000  # TODO delete (testing)
 
 
 class Gui:
     def __init__(self, screws_position: List[Tuple[float, float]]):
         self._screws_position = screws_position
 
-    def draw_strings(self, strings: List[List[int]]) -> None:
+    def draw_strings(self, strings: List[Tuple[int, int]]) -> None:
         for line in strings:
             self._draw_screws_string_connection(line[0], line[1])
 
@@ -36,14 +38,29 @@ class Gui:
 
 
 class Engine:
-    def __init__(self):
-        self._leftover_string = STRING_LENGTH
+    def __init__(self, image_path: str):
         self._screws_position = None
-        self.init_screws()
-        # TODO Engine should handle all image processing (blurring, quantisizing)
+        self._image = None
+        self._prepare_image(image_path)
+        self._init_screws()
 
-    def init_screws(self) -> None:
-        x0, y0 = (0, 0)
+    def _prepare_image(self, image_path: str) -> None:
+        # load image as grayscale
+        im = np.array(Image.open(image_path).convert('L'))
+        # crop image to circle center
+        center = np.floor(0.5 * np.array(im.shape)).astype(np.int32)
+        im_radius = min(center)
+        im_crop = im[center[0] - im_radius: center[0] + im_radius,
+                  center[1] - im_radius: center[1] + im_radius]
+        # scale to circle size
+        im = np.array(Image.fromarray(im_crop).resize((RADIUS * 2, RADIUS * 2)))
+        im = (20/255) * im # TODO consider
+        plt.imshow(im)  # TODO delete (testing)
+        self._image = im.copy()
+        # TODO consider quantisizing, blurring, normalizing to gray
+
+    def _init_screws(self) -> None:
+        x0, y0 = np.array(self._image.shape[::-1]) * 0.5
         rads = (2 * np.pi / SCREWS_AMOUNT) * np.arange(SCREWS_AMOUNT)
         xys = np.array([[x0 + RADIUS * np.sin(angle), y0 + RADIUS * np.cos(angle)]
                         for angle in rads])
@@ -53,11 +70,17 @@ class Engine:
         assert self._screws_position is not None, "screws not initialized"
         return self._screws_position.copy()
 
+    def get_image(self):
+        return self._image
+
     @staticmethod
-    def just_try(randomize: bool) -> List[List[int]]:
+    def just_try(randomize: bool, connected: bool) -> Union[List[List[int]], List[Tuple[int, int]]]:
         if randomize:
             amount = 500
-            return np.random.randint(SCREWS_AMOUNT, size=(amount, 2))
+            if connected:
+                return Engine.steps_to_tuples(np.random.randint(SCREWS_AMOUNT, size=amount))
+            else:
+                return np.random.randint(SCREWS_AMOUNT, size=(amount, 2))
         else:
             jump = 70
             return np.vstack([np.arange(SCREWS_AMOUNT - jump),
@@ -72,71 +95,122 @@ class Engine:
         euclidean_distance = np.sqrt(np.sum(np.power(p2 - p1, 2)))
         return euclidean_distance
 
-    def use_string(self, amount: float) -> None:
-        if self._leftover_string - amount < 0:
-            raise Exception("Used too much string")
-        self._leftover_string -= amount
+    @staticmethod
+    def steps_to_tuples(steps: List[int]) -> List[Tuple[int, int]]:
+        return list(zip(steps[:-1], steps[1:]))
 
 
 class Algo:  # TODO separate all classes to different files.
-    def __init__(self, image_path, screws_position):
-        self._screws_position = screws_position
-        self._curr_state = None
+    def __init__(self, engine):
+        self._leftover_string = STRING_LENGTH
+        self._engine = engine
+        self._screws_position = engine.get_screws_positions()
+        self._curr_state = engine.get_image().copy()
         self._grid = None
-        self._prepare_image(image_path)
-        self._init_grid()
+        self._steps = None
+        self._update_grid()
 
-    def _init_grid(self):
+    def _update_grid(self):
         assert self._curr_state is not None, "must load and prepare image before using grid"
-        im_height, im_width = self._curr_state.shape  # TODO assuming RGB
+        assert len(self._curr_state.shape) == 2, "assuming grayscale image"
+        im_height, im_width = self._curr_state.shape
         self._grid = interpolate.interp2d(np.arange(im_width), np.arange(im_height),
                                           self._curr_state)
 
-    def _prepare_image(self, image_path: str):
-        # load image as grayscale
-        im = np.array(Image.open(image_path).convert('L'))
-        # crop image to circle center
-        center = np.floor(0.5 * np.array(im.shape)).astype(np.int32)
-        im_radius = min(center)
-        im_crop = im[center[0] - im_radius: center[0] + im_radius,
-                  center[1] - im_radius: center[1] + im_radius]
-        # scale to circle size
-        im = np.array(Image.fromarray(im_crop).resize((RADIUS * 2, RADIUS * 2)))
-        self._curr_state = im.copy()
+    def _apply_string(self, from_screw: int, to_screw: int,
+                      xs: List[float], ys: List[float]) -> None:
+        amount = self._engine.calculate_string_usage(from_screw, to_screw)
+        if self._leftover_string - amount < 0:
+            print("raise Exception('Used too much string')")  # TODO decide
+        for point in zip(xs, ys):  # TODO optimize
+            col1, col2 = int(np.floor(point[0])), int(np.ceil(point[0]))
+            if col2 >= self._curr_state.shape[1]:
+                col2 = col1
+            if col1 == col2:
+                col1_val = 1
+                col2_val = 0
+            else:
+                col1_val = point[0] - col1
+                col2_val = col2 - point[0]
+            row1, row2 = int(np.floor(point[1])), int(np.ceil(point[1]))
+            if row2 >= self._curr_state.shape[0]:
+                row2 = row1
+            if row1 == row2:
+                row1_val = 1
+                row2_val = 0
+            else:
+                row1_val = point[1] - row1
+                row2_val = row2 - point[1]
+            if col1 >= self._curr_state.shape[1] or row1 >= self._curr_state.shape[0]:
+                print('bad...', col1, col2, row1, row2, from_screw, to_screw)
+                continue # TODO FIXME
+            self._curr_state[row1, col1] += 0.5 * (col1_val + row1_val)
+            self._curr_state[row1, col2] += 0.5 * (col2_val + row1_val)
+            self._curr_state[row2, col2] += 0.5 * (col2_val + row2_val)
+            self._curr_state[row2, col1] += 0.5 * (col1_val + row2_val)
+        self._leftover_string -= amount
+        self._update_grid()
+        print('\tleftover:', self._leftover_string)  # TODO delete
 
-    def get_next(self, current_screw: int):  # TODO check: state : Dict, -> int:
+    def get_next(self, current_screw: int) -> Tuple[int, List[float], List[float]]:
         if not (0 <= current_screw < SCREWS_AMOUNT):
             raise IndexError
-        best_score = self._score_line(current_screw, current_screw + 1)
+        best_candidate = current_screw
+        while best_candidate == current_screw:
+            best_candidate = (current_screw + np.random.randint(SCREWS_AMOUNT)) % SCREWS_AMOUNT
+        best_score, xs, ys = self._score_line(current_screw, best_candidate)
         for i in range(SCREWS_AMOUNT):
             if i in (current_screw, current_screw + 1):
                 continue
-            
+            score, xs, ys = self._score_line(current_screw, i)
+            if score > best_score:
+                best_candidate = i
+        return best_candidate, xs, ys
 
-        # TODO implement (cont)
-
-    def _score_line(self, screw1: int, screw2: int) -> float:
+    def _score_line(self, screw1: int, screw2: int) \
+            -> Tuple[float, List[float], List[float]]:
+        # TODO optimize by RectBivariateSpline
         p1, p2 = [np.array(self._screws_position[i]) for i in (screw1, screw2)]
         euclidean_distance = np.sqrt(np.sum(np.power(p2 - p1, 2)))
         xs = p1[0] + (p2[0] - p1[0]) * np.arange(np.ceil(euclidean_distance)) / euclidean_distance
         ys = p1[1] + (p2[1] - p1[1]) * np.arange(np.ceil(euclidean_distance)) / euclidean_distance
-        score = 0
-        for point in zip(xs, ys):
-            intensity = self._grid(*point)
-            score -= intensity
-            # TODO plan: after choosing line, increasing state's intensity by 1 (recreate grid), including increasing whites to 256 etc
-        return score
+        if len(xs) > 1 or len(ys) > 1:
+            intensities = self._grid(xs, ys).diagonal()
+        else:
+            intensities = self._grid(xs, ys)
+        score = (-1 * np.sum(intensities)) / euclidean_distance
+        return score, xs, ys
+
+    def execute(self) -> List[int]:
+        current_screw = np.random.randint(SCREWS_AMOUNT)
+        steps = [current_screw]
+        while self._leftover_string > 0:
+            next_screw, xs, ys = self.get_next(current_screw)
+            self._apply_string(current_screw, next_screw, xs, ys)
+            steps.append(next_screw)
+            current_screw = next_screw
+        return steps
+
+
+def main(image_path):
+    print("Running with string length", STRING_LENGTH / (100 * 1000),
+          "km, to create a circle with radius", RADIUS, 'cm, with', SCREWS_AMOUNT,
+          'screws.')
+    infrastructure_engine = Engine(image_path)
+    ui = Gui(infrastructure_engine.get_screws_positions())
+
+    begin_time = time.time()
+    algo = Algo(infrastructure_engine)
+    res_steps = algo.execute()
+    print('Time: ', time.time() - begin_time)
+    print(res_steps)
+
+    # trial = infrastructure_engine.just_try(randomize=True, connected=True)
+    ui.draw_screws()
+    ui.draw_strings(infrastructure_engine.steps_to_tuples(res_steps))
+    ui.show()
 
 
 if __name__ == '__main__':
-    engine = Engine()
-    screws_pos = engine.get_screws_positions()
-    ui = Gui(screws_pos)
-
     tux_path = '/home/ru/Pictures/tux-100677393-large.jpg'
-    algo = Algo(tux_path, screws_pos)
-
-    trial = engine.just_try(randomize=True)
-    ui.draw_screws()
-    ui.draw_strings(trial)
-    ui.show()
+    main(tux_path)
